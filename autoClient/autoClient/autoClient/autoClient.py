4d0a85e05b38e11d2336122a33124baea7f5dcec
hongjun kim <jun0683@gmail.com>
2012-04-11 01:30:12 +0900

    auto client init

---
+++ autoClient/autoClient/autoClient/autoClient.py
@@ -0,0 +1,22 @@
+class autoClient:
+    def __init__(self):
+        self.login()
+        self.logout()
+        self.makeChattingRoom()
+        self.inviteChattingRoom()
+        self.leaveChattingRoom()
+
+    def login(self):
+        print('login')
+
+    def logout(self):
+        print('login')
+
+    def makeChattingRoom(self):
+        print('makeChattingRoom')
+
+    def inviteChattingRoom(self):
+        print('inviteChattingRoom')
+
+    def leaveChattingRoom(self):
+        print('leaveChattingRoom')
---
+++ autoClient/autoClient/autoClient/autoClientMain.py
@@ -0,0 +1,9 @@
+from autoClient import autoClient
+
+
+def main():
+    print('start')
+    a = autoClient()
+
+if __name__ == '__main__':
+    main()
