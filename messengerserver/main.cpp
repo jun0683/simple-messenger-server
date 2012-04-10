e6c58cf65ad67f37f8daca4780dc3998fd90a3a1
hongjun kim <jun0683@gmail.com>
2012-04-11 00:58:31 +0900

    refactoring & add todo

--- messengerserver/main.cpp
+++ messengerserver/main.cpp
@@ -4,28 +4,16 @@
 #include "UserInfo.h"
 #include "testDBCode.h"
 
+void dbtest();
+
 
 
 int main(int argc, char* argv[])
 {
 	try
 	{
-		CDBManager dbmanager;
-		dbmanager.dbLogin();
-		testuserInfo(dbmanager);
-		testUserID(dbmanager);
-		testUserLogState(dbmanager);
-		testUserFriends(dbmanager);
-		testUserFriendsLoginState(dbmanager);
-		testCreatechattingRoom(dbmanager);
-		testSendChattingMessage(dbmanager);
-		testRegistNewUser(dbmanager);
-		testisValidUser(dbmanager);
-		testChageUserInfo(dbmanager);
-		testUserWithdraw(dbmanager);
-		testAddfriend(dbmanager);
-		testfindUserID(dbmanager);
-		testdelFriend(dbmanager);
+		dbtest();
+
 		
 	}
 	catch(std::exception &e)
@@ -33,4 +21,24 @@ int main(int argc, char* argv[])
 		std::cerr << e.what() << endl;
 	}
 	return 0;
+}
+
+void dbtest()
+{
+	CDBManager dbmanager;
+	dbmanager.dbLogin();
+	testuserInfo(dbmanager);
+	testUserID(dbmanager);
+	testUserLogState(dbmanager);
+	testUserFriends(dbmanager);
+	testUserFriendsLoginState(dbmanager);
+	testCreatechattingRoom(dbmanager);
+	testSendChattingMessage(dbmanager);
+	testRegistNewUser(dbmanager);
+	testisValidUser(dbmanager);
+	testChageUserInfo(dbmanager);
+	testUserWithdraw(dbmanager);
+	testAddfriend(dbmanager);
+	testfindUserID(dbmanager);
+	testdelFriend(dbmanager);
 }
